40912d9152e44aae8f2445d23a32ed67800dd9c7
Jeffrey Brice <jeffbricemusic@gmail.com>
2013-11-26 19:10:39 -0800

    Initial Commit

---
+++ Snitchy Tools/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Snitchy Tools
+//
+//  Created by Jeffrey Brice on 11/26/13.
+//  Copyright (c) 2013 Spacebooke Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Snitchy Tools/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Snitchy Tools
+//
+//  Created by Jeffrey Brice on 11/26/13.
+//  Copyright (c) 2013 Spacebooke Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
